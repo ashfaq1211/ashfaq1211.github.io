3bfae1a3b06eafc5b8d6db5ac4f5ca1e543dd737
ashfaq1211 <34036382+ashfaq1211@users.noreply.github.com>
2020-09-11 17:53:53 +0600

    file paths updated for MVC structure.

--- Controller/emailscript.php
+++ Controller/emailscript.php
@@ -7,50 +7,42 @@
    }
    $user = $_SESSION['user']; //assigns user value
 ?>
+
 <?php
-	$link = mysqli_connect("localhost", "root","") or die(mysql_error());
-	  mysqli_select_db($link, "rent_it_3") or die("Cannot connect to database");
-	  $temp = $_GET['id'];
-	  $sql = "SELECT * FROM posts WHERE post_id = '$temp'";
-	  $query = mysqli_query($link, $sql);
-	  $row = mysqli_fetch_array($query);
-	      //POST VALUES
-	      $userdisp = $row['user'];
-	      $id = $row['post_id'];
-	      $post = $row['sale_rent_lease'];
-	      $dur = $row['duration'];
-	      $acc = $row['acc_type'];
-	      $floor = $row['floor'];
-	      $flat = $row['flat'];
-	      $prop = $row['prop_size'];
-	      $direc = $row['direc_facing'];
-	      $area = $row['area'];
-	      $street = $row['street'];
-	      $house = $row['house'];
-	      $price = $row['price'];
-	      $add = $row['add_details'];
-	      $parking = $row['parking'];
-	      $lift = $row['lift'];
-	      $generator = $row['generator'];
-	      $date = $row['date_posted'];
-	      $time = $row['time_posted'];
-
-	      $sql2 = "SELECT * FROM users WHERE username = '$userdisp'";
-          $query = mysqli_query($link, $sql2);
-          $row = mysqli_fetch_array($query);
-          $email_poster = $row['email'];
-
-          $sql3 = "SELECT * FROM users WHERE username = '$user'";
-          $query = mysqli_query($link, $sql3);
-          $row = mysqli_fetch_array($query);
-          $email_responder = $row['email'];
-
-
-    $headers =  'MIME-Version: 1.0' . "\r\n"; 
-	$headers .= 'From: Your name <".$email_responder.">' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-    $subject = 'A Post On RENT IT Has Been Booked';
-    $message = 'Your post, ".$acc." for ".$post." at Road: ".$street.", ".$area.", on RENT IT has been booked. Please respond to the following email address: ".$emal_responder."';
-
-    mail($email_poster, $subject, $message, $headers);
+	session_start();
+	$link = mysqli_connect("localhost", "root","") or die(mysql_error()); //COnnect to server
+	$username = mysqli_real_escape_string($link, $_POST['username']);
+	$password = mysqli_real_escape_string($link, $_POST['password']);
+	mysqli_select_db($link, "rent_it_3") or die("Cannot connect to database"); //Connect to database
+	$query = mysqli_query($link, "SELECT * from users WHERE username='$username'"); //Query the users table if there are matching rows equal to $username
+	$exists = mysqli_num_rows($query); //Checks if username exists
+	$table_users = "";
+	$table_password = "";
+	if($exists > 0) //IF there are no returning rows or no existing username
+	{
+		while($row = mysqli_fetch_assoc($query)) //display all rows from query
+		{
+			$table_users = $row['username']; // the first username row is passed on to $table_users, and so on until the query is finished
+			$table_password = $row['password']; // the first password row is passed on to $table_users, and so on until the query is finished
+		if(($username == $table_users) && ($password == $table_password)) // checks if there are any matching fields
+		{
+				if($password == $table_password)
+				{
+					$_SESSION['user'] = $username; //set the username in a session. This serves as a global variable
+					header("location: ../../MVC/View/home.php"); // redirects the user to the authenticated home page
+				}
+				
+		}
+		else
+		{
+			Print '<script>alert("Incorrect Password!");</script>'; //Prompts the user
+			Print '<script>window.location.assign("../../MVC/View/login.php");</script>'; // redirects to login.php
+		}
+		}
+	}
+	else
+	{
+		Print '<script>alert("Incorrect Username!");</script>'; //Prompts the user
+		Print '<script>window.location.assign("../../MVC/View/login.php");</script>'; // redirects to login.php
+	}
 ?>
