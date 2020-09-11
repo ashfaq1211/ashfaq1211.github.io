f918364b96d153288aedef597e4b107849f23d05
ashfaq1211 <34036382+ashfaq1211@users.noreply.github.com>
2020-09-11 18:01:31 +0600

    file paths updated for MVC structure.

--- View/mydashboard.php
+++ View/mydashboard.php
@@ -62,13 +62,13 @@
             </ul>
             <ul class="navbar-nav ml-auto mt-10">
               <li class="nav-item">
-                <a class="nav-link login-button" href="logout.php">Logout</a>
+                <a class="nav-link login-button" href="../../MVC/Controller/logout.php">Logout</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link add-button" href="post.php"><i class="fa fa-plus-circle"></i> Post Ad</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link login-button" href="deleteaccount.php">Delete Account</a>
+                <a class="nav-link login-button" href="../../MVC/Controller/deleteaccount.php">Delete Account</a>
               </li>
             </ul>
           </div>
@@ -187,7 +187,7 @@
                var r = confirm("Are you sure you want to delete this record?");
                if(r == true)
                {
-                  window.location.assign("deletepost.php?id=" + id);
+                  window.location.assign("../../MVC/Controller/deletepost.php?id=" + id);
                }
             }
         </script>
